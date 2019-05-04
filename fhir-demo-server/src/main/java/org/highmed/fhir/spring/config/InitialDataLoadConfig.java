ae13b16587f47527c0b85969cf4c1e88c8996787
Hauke Hund <hauke.hund@hs-heilbronn.de>
2019-05-04 23:31:27 +0200

    jar internal bundle.xml file to load software release managed resources
    jar internal bundle should be used to load StructureDefinitions,
    CodeSystem, etc. managed via software release cycles. Resource managed
    via local users oder TTP should be part of external bundle.xml file

--- fhir-demo-server/src/main/java/org/highmed/fhir/spring/config/InitialDataLoadConfig.java
+++ fhir-demo-server/src/main/java/org/highmed/fhir/spring/config/InitialDataLoadConfig.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 
 import org.highmed.fhir.init.InitialDataLoader;
 import org.hl7.fhir.r4.model.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +20,8 @@ import org.springframework.context.event.EventListener;
 @Configuration
 public class InitialDataLoadConfig
 {
+	private static final Logger logger = LoggerFactory.getLogger(InitialDataLoadConfig.class);
+
 	@Value("${org.highmed.fhir.init.bundle.file}")
 	private String initBundleFile;
 
@@ -36,12 +40,20 @@ public class InitialDataLoadConfig
 	@EventListener({ ContextRefreshedEvent.class })
 	public void onContextRefreshedEvent(ContextRefreshedEvent event) throws IOException
 	{
+		try (InputStream fileIn = ClassLoader.getSystemResourceAsStream("fhir/bundle.xml"))
+		{
+			logger.debug("Loading data from JAR bundle ...");
+			Bundle bundle = fhirConfig.fhirContext().newXmlParser().parseResource(Bundle.class, fileIn);
+			initialDataLoader().load(bundle);
+		}
+
 		Path file = Paths.get(initBundleFile);
 		if (!Files.isReadable(file))
 			throw new IOException("Init bundle file at " + file.toString() + " not readable");
 
 		try (InputStream fileIn = Files.newInputStream(file))
 		{
+			logger.debug("Loading data from external bundle ...");
 			Bundle bundle = fhirConfig.fhirContext().newXmlParser().parseResource(Bundle.class, fileIn);
 			initialDataLoader().load(bundle);
 		}
