9e98131614402e62d8099631be6c63e199e28d22
Hauke Hund <hauke.hund@hs-heilbronn.de>
2019-06-15 23:47:33 +0200

    fixed copy/paste error

--- dsf-docker-test-setup-3medic-ttp/docker-build-tag-push.bat
+++ dsf-docker-test-setup-3medic-ttp/docker-build-tag-push.bat
@@ -2,12 +2,20 @@
 
 echo highmed/bpe ...
 docker build -t highmed/bpe ..\dsf-bpe\dsf-bpe-server-jetty
+docker tag highmed/bpe:latest registry:5000/highmed/bpe:latest
+docker push registry:5000/highmed/bpe
 
 echo highmed/bpe_proxy ...
 docker build -t highmed/bpe_proxy ..\dsf-docker\bpe_proxy
+docker tag highmed/bpe_proxy:latest registry:5000/highmed/bpe_proxy:latest
+docker push registry:5000/highmed/bpe_proxy
 
 echo highmed/fhir ...
 docker build -t highmed/fhir ..\dsf-fhir\dsf-fhir-server-jetty
+docker tag highmed/fhir:latest registry:5000/highmed/fhir:latest
+docker push registry:5000/highmed/fhir
 
 echo highmed/fhir_proxy ...
 docker build -t highmed/fhir_proxy ..\dsf-docker\fhir_proxy
+docker tag highmed/fhir_proxy:latest registry:5000/highmed/fhir_proxy:latest
+docker push registry:5000/highmed/fhir_proxy
