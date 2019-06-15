ef9d541653a18b7033680e348a4180e67f7ff64c
Hauke Hund <hauke.hund@hs-heilbronn.de>
2019-06-15 22:38:45 +0200

    docker build and docker build/tag/push batch files

---
+++ dsf-docker-test-setup-3medic-ttp/docker-build-tag-push.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo highmed/bpe ...
+docker build -t highmed/bpe ..\dsf-bpe\dsf-bpe-server-jetty
+
+echo highmed/bpe_proxy ...
+docker build -t highmed/bpe_proxy ..\dsf-docker\bpe_proxy
+
+echo highmed/fhir ...
+docker build -t highmed/fhir ..\dsf-fhir\dsf-fhir-server-jetty
+
+echo highmed/fhir_proxy ...
+docker build -t highmed/fhir_proxy ..\dsf-docker\fhir_proxy
