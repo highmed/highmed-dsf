40e7976fea39d11cbb5d5d6b7e030c210d922862
Hauke Hund <hauke.hund@hs-heilbronn.de>
2019-07-29 16:05:09 +0200

    non standard ContextLoaderListener to enable integration test
    integration tests need access to spring beans via webapplication context

--- dsf-fhir/dsf-fhir-server/src/main/java/org/highmed/dsf/fhir/FhirSpringApplication.java
+++ dsf-fhir/dsf-fhir-server/src/main/java/org/highmed/dsf/fhir/FhirSpringApplication.java
@@ -4,7 +4,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
@@ -14,7 +13,7 @@ public class FhirSpringApplication implements WebApplicationInitializer
 	public void onStartup(ServletContext servletContext) throws ServletException
 	{
 		AnnotationConfigWebApplicationContext context = getContext();
-		servletContext.addListener(new ContextLoaderListener(context));
+		servletContext.addListener(new FhirContextLoaderListener(context));
 		servletContext.addListener(new RequestContextListener());
 	}
 
