0eab0f848387b40d85be2ac3917cfe961b2a3cd9
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2019-08-08 18:43:19 +0200

    adds sceleton for simple feasibility process

--- dsf-bpe/dsf-bpe-process-simple-feasibility/src/main/java/org/highmed/dsf/bpe/plugin/SimpleFeasibilityPlugin.java
+++ dsf-bpe/dsf-bpe-process-simple-feasibility/src/main/java/org/highmed/dsf/bpe/plugin/SimpleFeasibilityPlugin.java
@@ -1,16 +1,22 @@
 package org.highmed.dsf.bpe.plugin;
 
 import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
-import org.highmed.dsf.bpe.listener.DefaultBpmnParseListener;
 
 public class SimpleFeasibilityPlugin extends AbstractProcessEnginePlugin
 {
 	private static final String REQUEST_FILE = "requestSimpleCohortSizeQuery.bpmn";
 	private static final String EXECUTION_FILE = "executeSimpleCohortSizeQuery.bpmn";
 
-	public SimpleFeasibilityPlugin(DefaultBpmnParseListener defaultBpmnParseListener) {
-		super(defaultBpmnParseListener);
+	@Override
+	public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration)
+	{
+	}
+
+	@Override
+	public void postInit(ProcessEngineConfigurationImpl processEngineConfiguration)
+	{
 	}
 
 	@Override
