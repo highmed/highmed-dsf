highmed/highmed-dsf

commit 081a47a32d64d62df3073e711a34f7a1c55d1297

adds output and input slices of feasibility to profiles and adds profile url to sent tasks

dsf-bpe/dsf-bpe-process-base/src/main/java/org/highmed/dsf/fhir/task/AbstractTaskMessageSend.java
@@ -13,6 +13,7 @@ import org.highmed.dsf.fhir.variables.MultiInstanceTarget;
 import org.highmed.fhir.client.WebserviceClient;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Meta;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.StringType;
 import org.hl7.fhir.r4.model.Task;
@@ -50,6 +51,7 @@ public class AbstractTaskMessageSend extends AbstractServiceDelegate implements 
 		String processDefinitionKey = (String) execution.getVariable(Constants.VARIABLE_PROCESS_DEFINITION_KEY);
 		String versionTag = (String) execution.getVariable(Constants.VARIABLE_VERSION_TAG);
 		String messageName = (String) execution.getVariable(Constants.VARIABLE_MESSAGE_NAME);
+		String profile = (String) execution.getVariable(Constants.VARIABLE_MESSAGE_NAME);
 		String businessKey = execution.getBusinessKey();
 
 		// TODO see Bug https://app.camunda.com/jira/browse/CAM-9444
@@ -60,7 +62,7 @@ public class AbstractTaskMessageSend extends AbstractServiceDelegate implements 
 				.getVariable(Constants.VARIABLE_MULTI_INSTANCE_TARGET);
 
 		sendTask(target.getTargetOrganizationIdentifierValue(), processDefinitionKey, versionTag, messageName,
-				businessKey, target.getCorrelationKey(), getAdditionalInputParameters(execution));
+				businessKey, profile, target.getCorrelationKey(), getAdditionalInputParameters(execution));
 	}
 
 	/**
@@ -74,13 +76,14 @@ public class AbstractTaskMessageSend extends AbstractServiceDelegate implements 
 	}
 
 	protected void sendTask(String targetOrganizationIdentifierValue, String processDefinitionKey, String versionTag,
-			String messageName, String businessKey, String correlationKey,
+			String messageName, String businessKey, String correlationKey, String profile,
 			Stream<ParameterComponent> additionalInputParameters)
 	{
 		if (messageName.isEmpty() || processDefinitionKey.isEmpty())
 			throw new IllegalStateException("Next process-id or message-name not definied");
 
 		Task task = new Task();
+		task.setMeta(new Meta().addProfile(profile));
 		task.setStatus(TaskStatus.REQUESTED);
 		task.setIntent(TaskIntent.ORDER);
 		task.setAuthoredOn(new Date());

dsf-bpe/dsf-bpe-process-base/src/main/java/org/highmed/dsf/bpe/Constants.java
@@ -5,6 +5,7 @@ public interface Constants
 	String VARIABLE_MESSAGE_NAME = "messageName";
 	String VARIABLE_PROCESS_DEFINITION_KEY = "processDefinitionKey";
 	String VARIABLE_VERSION_TAG = "versionTag";
+	String VARIABLE_PROFILE = "profile";
 	// String VARIABLE_TARGET_ORGANIZATION_ID = "targetOrganizationId";
 	// String VARIABLE_CORRELATION_KEY = "correlationKey";
 	String VARIABLE_MULTI_INSTANCE_TARGET = "multiInstanceTarget";

dsf-bpe/dsf-bpe-process-feasibility/src/main/resources/executeSimpleFeasibility.bpmn
@@ -31,6 +31,7 @@
           <camunda:inputParameter name="messageName">resultSimpleFeasibilityMessage</camunda:inputParameter>
           <camunda:inputParameter name="versionTag">1.0.0</camunda:inputParameter>
           <camunda:inputParameter name="processDefinitionKey">requestSimpleFeasibility</camunda:inputParameter>
+          <camunda:inputParameter name="profile">http://highmed.org/fhir/StructureDefinition/highmed-task-result-simple-feasibility</camunda:inputParameter>
         </camunda:inputOutput>
       </bpmn:extensionElements>
       <bpmn:incoming>SequenceFlow_1gynemm</bpmn:incoming>

dsf-bpe/dsf-bpe-process-feasibility/src/main/resources/requestSimpleFeasibility.bpmn
@@ -19,6 +19,7 @@
           <camunda:inputParameter name="processDefinitionKey">executeSimpleFeasibility</camunda:inputParameter>
           <camunda:inputParameter name="versionTag">1.0.0</camunda:inputParameter>
           <camunda:inputParameter name="messageName">executeSimpleFeasibilityMessage</camunda:inputParameter>
+          <camunda:inputParameter name="profile">http://highmed.org/fhir/StructureDefinition/highmed-task-execute-simple-feasibility</camunda:inputParameter>
         </camunda:inputOutput>
       </bpmn:extensionElements>
       <bpmn:incoming>SequenceFlow_0b5s4ef</bpmn:incoming>

dsf-fhir/dsf-fhir-server/src/main/resources/fhir/StructureDefinition/highmed-task-request-simple-feasibility-0.1.0.xml
@@ -154,7 +154,7 @@
         <targetProfile value="http://highmed.org/fhir/StructureDefinition/highmed-research-study" />
       </type>
     </element>
-   <element id="Task.output">
+    <element id="Task.output">
       <path value="Task.output" />
       <slicing>
         <discriminator>
@@ -177,6 +177,42 @@
       <fixedCode value="error" />
     </element>
     <element id="Task.output:error.value[x]">
+      <path value="Task.output.value[x]" />
+      <type>
+        <code value="string" />
+      </type>
+    </element>
+    <element id="Task.output:participatingMedics">
+      <path value="Task.output" />
+      <sliceName value="participatingMedics" />
+    </element>
+    <element id="Task.output:participatingMedics.type.coding.system">
+      <path value="Task.output.type.coding.system" />
+      <fixedUri value="http://highmed.org/fhir/NamingSystem/feasibility" />
+    </element>
+    <element id="Task.output:participatingMedics.type.coding.code">
+      <path value="Task.output.type.coding.code" />
+      <patternCode value="participatingMedics|Group/[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}" />
+    </element>
+    <element id="Task.output:participatingMedics.value[x]">
+      <path value="Task.output.value[x]" />
+      <type>
+        <code value="string" />
+      </type>
+    </element>
+    <element id="Task.output:cohortSize">
+      <path value="Task.output" />
+      <sliceName value="cohortSize" />
+    </element>
+    <element id="Task.output:cohortSize.type.coding.system">
+      <path value="Task.output.type.coding.system" />
+      <fixedUri value="http://highmed.org/fhir/NamingSystem/feasibility" />
+    </element>
+    <element id="Task.output:cohortSize.type.coding.code">
+      <path value="Task.output.type.coding.code" />
+      <patternCode value="cohortSize|Group/[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}" />
+    </element>
+    <element id="Task.output:cohortSize.value[x]">
       <path value="Task.output.value[x]" />
       <type>
         <code value="string" />

dsf-fhir/dsf-fhir-server/src/main/resources/fhir/StructureDefinition/highmed-task-result-simple-feasibility-0.1.0.xml
@@ -133,28 +133,26 @@
         <code value="string" />
       </type>
     </element>
-    <element id="Task.input:research-study">
+    <element id="Task.input:singleResult">
       <path value="Task.input" />
-      <sliceName value="research-study" />
+      <sliceName value="singleResult" />
       <min value="1" />
-      <max value="1" />
     </element>
-    <element id="Task.input:research-study.type.coding.system">
+    <element id="Task.input:singleResult.type.coding.system">
       <path value="Task.input.type.coding.system" />
-      <fixedUri value="http://highmed.org/fhir/CodeSystem/task-input" />
+      <fixedUri value="http://highmed.org/fhir/NamingSystem/feasibility" />
     </element>
-    <element id="Task.input:research-study.type.coding.code">
+    <element id="Task.input:singleResult.type.coding.code">
       <path value="Task.input.type.coding.code" />
-      <fixedCode value="research-study-reference" />
+      <patternCode value="singelResult|Group/[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}" />
     </element>
-    <element id="Task.input:research-study.value[x]">
+    <element id="Task.input:singleResult.value[x]">
       <path value="Task.input.value[x]" />
       <type>
-        <code value="Reference" />
-        <targetProfile value="http://highmed.org/fhir/StructureDefinition/highmed-research-study" />
+        <code value="string" />
       </type>
     </element>
-   <element id="Task.output">
+    <element id="Task.output">
       <path value="Task.output" />
       <slicing>
         <discriminator>

commit 185a651d0422a93038761619a298ea8e0a22b965

fixes correlation error introduced in 081a47a32d64d62df3073e711a34f7a1c55d1297

dsf-bpe/dsf-bpe-process-base/src/main/java/org/highmed/dsf/fhir/task/AbstractTaskMessageSend.java
@@ -62,7 +62,7 @@ public class AbstractTaskMessageSend extends AbstractServiceDelegate implements 
 				.getVariable(Constants.VARIABLE_MULTI_INSTANCE_TARGET);
 
 		sendTask(target.getTargetOrganizationIdentifierValue(), processDefinitionKey, versionTag, messageName,
-				businessKey, profile, target.getCorrelationKey(), getAdditionalInputParameters(execution));
+				businessKey, target.getCorrelationKey(), profile, getAdditionalInputParameters(execution));
 	}
 
 	/**
@@ -83,7 +83,7 @@ public class AbstractTaskMessageSend extends AbstractServiceDelegate implements 
 			throw new IllegalStateException("Next process-id or message-name not definied");
 
 		Task task = new Task();
-		task.setMeta(new Meta().addProfile(profile));
+		//task.setMeta(new Meta().addProfile(profile));
 		task.setStatus(TaskStatus.REQUESTED);
 		task.setIntent(TaskIntent.ORDER);
 		task.setAuthoredOn(new Date());