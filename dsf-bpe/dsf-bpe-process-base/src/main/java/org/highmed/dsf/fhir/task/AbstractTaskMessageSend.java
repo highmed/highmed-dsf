090d31ec7316a23e04ef1f9842df6a9764179fe6
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2019-09-06 09:13:32 +0200

    adapts ping process to new task handling architecture, adds ping fhir profiles, adapts feasibility fhir profiles to be derived from base task fhir profile

--- dsf-bpe/dsf-bpe-process-base/src/main/java/org/highmed/dsf/fhir/task/AbstractTaskMessageSend.java
+++ dsf-bpe/dsf-bpe-process-base/src/main/java/org/highmed/dsf/fhir/task/AbstractTaskMessageSend.java
@@ -83,7 +83,7 @@ public class AbstractTaskMessageSend extends AbstractServiceDelegate implements 
 			throw new IllegalStateException("Next process-id or message-name not definied");
 
 		Task task = new Task();
-		//task.setMeta(new Meta().addProfile(profile));
+		task.setMeta(new Meta().addProfile(profile));
 		task.setStatus(TaskStatus.REQUESTED);
 		task.setIntent(TaskIntent.ORDER);
 		task.setAuthoredOn(new Date());
