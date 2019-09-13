a65a817e18fc94e188435deaf13bd669ad62375f
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2019-09-13 11:46:25 +0200

    adds client values to conf files and changes deserializer

--- dsf-openehr/dsf-openehr-model/src/main/java/org/highmed/openehr/deserializer/RowElementDeserializer.java
+++ dsf-openehr/dsf-openehr-model/src/main/java/org/highmed/openehr/deserializer/RowElementDeserializer.java
@@ -3,24 +3,16 @@ package org.highmed.openehr.deserializer;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import org.highmed.openehr.model.datatypes.DV_Count;
 import org.highmed.openehr.model.datatypes.DV_Other;
 import org.highmed.openehr.model.structur.RowElement;
 
 import java.io.IOException;
 
-public class RowElementDeserializer extends StdDeserializer<RowElement> {
-
-    public RowElementDeserializer() {
-        this(null);
-    }
-
-    public RowElementDeserializer(Class<?> vc) {
-        super(vc);
-    }
-
+public class RowElementDeserializer extends JsonDeserializer<RowElement>
+{
     @Override
     public RowElement deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
         JsonNode node = jsonParser.getCodec().readTree(jsonParser);
