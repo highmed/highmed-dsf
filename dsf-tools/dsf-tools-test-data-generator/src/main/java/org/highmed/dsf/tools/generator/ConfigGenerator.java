274aa8731734cfcf69a71e5c96f1dce560a15cfe
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2019-10-24 17:31:44 +0200

    adds cors property to property templates

--- dsf-tools/dsf-tools-test-data-generator/src/main/java/org/highmed/dsf/tools/generator/ConfigGenerator.java
+++ dsf-tools/dsf-tools-test-data-generator/src/main/java/org/highmed/dsf/tools/generator/ConfigGenerator.java
@@ -69,6 +69,8 @@ public class ConfigGenerator
 				testClient.getCertificateSha512ThumbprintHex() + ","
 						+ webbrowserTestUser.getCertificateSha512ThumbprintHex());
 
+		System.out.println("PPPP " + javaTestFhirConfigProperties);
+
 		writeProperties(Paths.get("config/java-test-fhir-config.properties"), javaTestFhirConfigProperties);
 	}
 
