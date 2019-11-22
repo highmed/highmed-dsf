b70c20765cba50f23104618b6337a484d7f3a143
Hauke Hund <hauke.hund@hs-heilbronn.de>
2019-11-22 18:56:33 +0100

    hapi fhir upgrade to 4.1.0, changes to validation related tests

--- dsf-fhir/dsf-fhir-server/src/test/java/org/highmed/dsf/fhir/hapi/StructureDefinitionTreeTest.java
+++ dsf-fhir/dsf-fhir-server/src/test/java/org/highmed/dsf/fhir/hapi/StructureDefinitionTreeTest.java
@@ -39,7 +39,7 @@ public class StructureDefinitionTreeTest
 		FhirContext context = FhirContext.forR4();
 
 		Map<String, StructureDefinition> structureDefinitionsByUrl = Files
-				.list(Paths.get("src/test/resources/profiles"))
+				.list(Paths.get("src/test/resources/profiles")).filter(Files::isRegularFile)
 				.map(p -> readStructureDefinition(context, p).setSnapshot(null))
 				.collect(Collectors.toMap(StructureDefinition::getUrl, Function.identity()));
 
