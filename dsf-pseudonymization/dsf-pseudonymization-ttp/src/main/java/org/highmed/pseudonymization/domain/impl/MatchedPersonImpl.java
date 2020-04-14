c353c699715a48347f54ef8959d16f13b0c170ee
Hauke Hund <hauke.hund@hs-heilbronn.de>
2020-04-14 20:42:16 +0200

    removed not needed code and interface MatchedPersonWithMdat

--- dsf-pseudonymization/dsf-pseudonymization-ttp/src/main/java/org/highmed/pseudonymization/domain/impl/MatchedPersonImpl.java
+++ dsf-pseudonymization/dsf-pseudonymization-ttp/src/main/java/org/highmed/pseudonymization/domain/impl/MatchedPersonImpl.java
@@ -5,38 +5,27 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import org.highmed.pseudonymization.domain.MatchedPersonWithMdat;
-import org.highmed.pseudonymization.domain.MdatContainer;
 import org.highmed.pseudonymization.domain.PersonWithMdat;
+import org.highmed.pseudonymization.recordlinkage.MatchedPerson;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class MatchedPersonImpl implements MatchedPersonWithMdat
+public class MatchedPersonImpl implements MatchedPerson<PersonWithMdat>
 {
 	private final List<PersonWithMdat> matches = new ArrayList<>();
-	private final List<MdatContainer> mdatContainers = new ArrayList<>();
 
 	public MatchedPersonImpl(PersonWithMdat person)
 	{
 		if (person != null)
-		{
 			matches.add(person);
-			
-			if (person.getMdatContainer() != null)
-				mdatContainers.add(person.getMdatContainer());
-		}
 	}
 
 	@JsonCreator
-	public MatchedPersonImpl(@JsonProperty("matches") Collection<? extends PersonWithMdat> matches,
-			@JsonProperty("mdatContainers") Collection<? extends MdatContainer> mdatContainers)
+	public MatchedPersonImpl(@JsonProperty("matches") Collection<? extends PersonWithMdat> matches)
 	{
 		if (matches != null)
 			this.matches.addAll(matches);
-
-		if (mdatContainers != null)
-			this.mdatContainers.addAll(mdatContainers);
 	}
 
 	@Override
@@ -50,11 +39,5 @@ public class MatchedPersonImpl implements MatchedPersonWithMdat
 	{
 		if (person != null)
 			matches.add(person);
-	}
-
-	@Override
-	public List<MdatContainer> getMdatContainers()
-	{
-		return Collections.unmodifiableList(mdatContainers);
 	}
 }
