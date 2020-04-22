cc0db0dc5e5b1b2a5ca5494a8bdf7e3ce0fb8256
Chris Heidt <christian.heidt@hs-heilbronn.de>
2020-04-22 10:25:26 +0200

    Cleanup of MatchingTimeTest, parameterizing the hash methods

--- dsf-pseudonymization/dsf-pseudonymization-ttp/src/test/java/org/highmed/pseudonymization/recordlinkage/MatchingTimeTest.java
+++ dsf-pseudonymization/dsf-pseudonymization-ttp/src/test/java/org/highmed/pseudonymization/recordlinkage/MatchingTimeTest.java
@@ -2,7 +2,6 @@ package org.highmed.pseudonymization.recordlinkage;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
@@ -13,99 +12,89 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.highmed.pseudonymization.bloomfilter.BloomFilterGenerator;
 import org.highmed.pseudonymization.bloomfilter.FieldBloomFilter;
 import org.highmed.pseudonymization.bloomfilter.RecordBloomFilter;
 import org.highmed.pseudonymization.domain.IdatImpl;
-import org.highmed.pseudonymization.domain.PersonWithMdat;
-import org.highmed.pseudonymization.domain.impl.MatchedPersonImpl;
 import org.highmed.pseudonymization.domain.impl.MedicIdImpl;
-import org.highmed.pseudonymization.domain.impl.OpenEhrMdatContainer;
-import org.highmed.pseudonymization.domain.impl.PersonImpl;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MatchingTimeTest
 {
 
+	private static final Logger logger = LoggerFactory.getLogger(MatchingTimeTest.class);
 	private static final int BIT_SET_LENGTH = 500;
 	private static final int BIT_SET_LENGTH_MEDIUM = 250;
 	private static final int BIT_SET_LENGTH_SHORT = 50;
 	private static List<IdatImpl> org1, org2 = new ArrayList<>();
 	private static ConcurrentHashMap<List<Double>, List<Double>> results = new ConcurrentHashMap<>();
-	//	private static LocalDateTime starttime, rbftime, endtime;
 	private static List<Duration> rbftimes = new ArrayList<>();
 	private static List<Duration> rltimes = new ArrayList<>();
 
 	@BeforeClass
 	public static void readFromTwoOrgs()
 	{
-		//org1 = read2(Paths.get("src/test/resources/dfD_org.csv"));
-		//org1 = read2(Paths.get("src/test/resources/singlept.csv"));
-		//org1 = read2(Paths.get("src/test/resources/df5k_org.csv"));
 		org1 = readWithTrueNeg(Paths.get("src/test/resources/df5k_org.csv"));
 
-		//org2 = read2(Paths.get("src/test/resources/dfD_dup.csv"));
-		//org2 = read2(Paths.get("src/test/resources/singlept_dup.csv"));
 		org2 = read2(Paths.get("src/test/resources/df5k_dup.csv"));
 	}
 
-	@AfterClass
-	public static void writeResults() {
-		String[] header = { "RBFTime", "LinkageTime" };
-		FileWriter out = null;
-		try
-		{
-			out = new FileWriter("src/test/resources/results_5k_sha1_sha2_32bHmacKey_TrueNeg_testtime.csv");
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		try (CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(header)))
-		{
-//			results.entrySet().forEach(result -> {
-//				try
-//				{
-//					List<Double> resultList = new ArrayList<>();
-//					resultList.addAll(result.getKey());
-//					resultList.addAll(result.getValue());
-//					printer.printRecord(resultList);
-//				}
-//				catch (IOException e)
-//				{
-//					e.printStackTrace();
-//				}
-//			});
-			printer.print("AVG Duration BFGen: " + calculateAvgDuration(rbftimes));
-			printer.print("Duration RL: " + calculateAvgDuration(rltimes));
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+	@Test
+	public void testTimeWithMd5Sha1()
+	{
+		BloomFilterGenerator generatorLong = BloomFilterGenerator.withMd5Sha1BiGramHasher(BIT_SET_LENGTH);
+		BloomFilterGenerator generatorMedium = BloomFilterGenerator.withMd5Sha1BiGramHasher(BIT_SET_LENGTH_MEDIUM);
+		BloomFilterGenerator generatorShort = BloomFilterGenerator.withMd5Sha1BiGramHasher(BIT_SET_LENGTH_SHORT);
+
+		testRuntime(generatorLong, generatorMedium, generatorShort);
+		logTimes("Md5", "SHA1");
 	}
 
 	@Test
-	public void testTwoOrgsOneWeightParallel() //To get more realistic times than with the parametrized Test
+	public void testTimeWithMd5Sha1Hmac()
 	{
-		LocalDateTime starttime = LocalDateTime.now();
+		Random rand = new Random(42L);
+		byte[] key1 = new byte[32];
+		byte[] key2 = new byte[32];
+		rand.nextBytes(key1);
+		rand.nextBytes(key2);
 
-		List<TestPerson> org1Pts = new ArrayList<>();
-		List<TestPerson> org2Pts = new ArrayList<>();
-		List<Double> weightList = Arrays.asList(0.1,0.1,0.2,0.1,0.1,0.05,0.2,0.1,0.05); // Best weights for SHA23Hmac, MD5SHA1
+		BloomFilterGenerator generatorLong = BloomFilterGenerator
+				.withHmacMd5HmacSha1BiGramHasher(BIT_SET_LENGTH, key1, key2);
+		BloomFilterGenerator generatorMedium = BloomFilterGenerator
+				.withHmacMd5HmacSha1BiGramHasher(BIT_SET_LENGTH_MEDIUM, key1, key2);
+		BloomFilterGenerator generatorShort = BloomFilterGenerator
+				.withHmacMd5HmacSha1BiGramHasher(BIT_SET_LENGTH_SHORT, key1, key2);
 
+		testRuntime(generatorLong, generatorMedium, generatorShort);
+		logTimes("Md5", "SHA1 HMAC");
+	}
+
+	@Test
+	public void testTimeWithSha1Sha2()
+	{
+		BloomFilterGenerator generatorLong = BloomFilterGenerator.withSha1Sha2BiGramHasher(BIT_SET_LENGTH);
+		BloomFilterGenerator generatorMedium = BloomFilterGenerator.withSha1Sha2BiGramHasher(BIT_SET_LENGTH_MEDIUM);
+		BloomFilterGenerator generatorShort = BloomFilterGenerator.withSha1Sha2BiGramHasher(BIT_SET_LENGTH_SHORT);
+
+		testRuntime(generatorLong, generatorMedium, generatorShort);
+		logTimes("SHA1", "SHA2");
+	}
+
+	@Test
+	public void testTimeWithSha1Sha2Hmac()
+	{
 		BouncyCastleProvider bcProvider = new BouncyCastleProvider();
 
 		Random rand = new Random(42L);
@@ -114,18 +103,6 @@ public class MatchingTimeTest
 		rand.nextBytes(key1);
 		rand.nextBytes(key2);
 
-//						BloomFilterGenerator generatorLong = BloomFilterGenerator.withMd5Sha1BiGramHasher(BIT_SET_LENGTH);
-//						BloomFilterGenerator generatorMedium = BloomFilterGenerator.withMd5Sha1BiGramHasher(BIT_SET_LENGTH_MEDIUM);
-//						BloomFilterGenerator generatorShort = BloomFilterGenerator.withMd5Sha1BiGramHasher(BIT_SET_LENGTH_SHORT);
-
-//		BloomFilterGenerator generatorLong = BloomFilterGenerator.withHmacMd5HmacSha1BiGramHasher(BIT_SET_LENGTH, key1, key2);
-//		BloomFilterGenerator generatorMedium = BloomFilterGenerator.withHmacMd5HmacSha1BiGramHasher(BIT_SET_LENGTH_MEDIUM, key1, key2);
-//		BloomFilterGenerator generatorShort = BloomFilterGenerator.withHmacMd5HmacSha1BiGramHasher(BIT_SET_LENGTH_SHORT, key1, key2);
-
-//		BloomFilterGenerator generatorLong = BloomFilterGenerator.withSha1Sha2BiGramHasher(BIT_SET_LENGTH);
-//		BloomFilterGenerator generatorMedium = BloomFilterGenerator.withSha1Sha2BiGramHasher(BIT_SET_LENGTH_MEDIUM);
-//		BloomFilterGenerator generatorShort = BloomFilterGenerator.withSha1Sha2BiGramHasher(BIT_SET_LENGTH_SHORT);
-
 		BloomFilterGenerator generatorLong = BloomFilterGenerator
 				.withHmacSha1HmacSha2BiGramHasher(BIT_SET_LENGTH, key1, key2, bcProvider);
 		BloomFilterGenerator generatorMedium = BloomFilterGenerator
@@ -133,19 +110,52 @@ public class MatchingTimeTest
 		BloomFilterGenerator generatorShort = BloomFilterGenerator
 				.withHmacSha1HmacSha2BiGramHasher(BIT_SET_LENGTH_SHORT, key1, key2, bcProvider);
 
-//						BloomFilterGenerator generatorLong = BloomFilterGenerator.withSha2Sha3BiGramHasher(BIT_SET_LENGTH);
-//						BloomFilterGenerator generatorMedium = BloomFilterGenerator.withSha2Sha3BiGramHasher(BIT_SET_LENGTH_MEDIUM);
-//						BloomFilterGenerator generatorShort = BloomFilterGenerator.withSha2Sha3BiGramHasher(BIT_SET_LENGTH_SHORT);
+		testRuntime(generatorLong, generatorMedium, generatorShort);
+		logTimes("SHA1 HMAC", "SHA2 HMAC");
+	}
 
-//				BloomFilterGenerator generatorLong = BloomFilterGenerator
-//						.withHmacSha2HmacSha3BiGramHasher(BIT_SET_LENGTH, key1, key2,
-//								bcProvider);
-//				BloomFilterGenerator generatorMedium = BloomFilterGenerator
-//						.withHmacSha2HmacSha3BiGramHasher(BIT_SET_LENGTH_MEDIUM, key1, key2,
-//								bcProvider);
-//				BloomFilterGenerator generatorShort = BloomFilterGenerator
-//						.withHmacSha2HmacSha3BiGramHasher(BIT_SET_LENGTH_SHORT, key1, key2,
-//								bcProvider);
+	@Test
+	public void testTimeWithSha2Sha3()
+	{
+		BloomFilterGenerator generatorLong = BloomFilterGenerator.withSha2Sha3BiGramHasher(BIT_SET_LENGTH);
+		BloomFilterGenerator generatorMedium = BloomFilterGenerator.withSha2Sha3BiGramHasher(BIT_SET_LENGTH_MEDIUM);
+		BloomFilterGenerator generatorShort = BloomFilterGenerator.withSha2Sha3BiGramHasher(BIT_SET_LENGTH_SHORT);
+
+		testRuntime(generatorLong, generatorMedium, generatorShort);
+		logTimes("SHA2", "SHA3");
+	}
+
+	@Test
+	public void testTimeWithSha2Sha3Hmac()
+	{
+		BouncyCastleProvider bcProvider = new BouncyCastleProvider();
+
+		Random rand = new Random(42L);
+		byte[] key1 = new byte[32];
+		byte[] key2 = new byte[32];
+		rand.nextBytes(key1);
+		rand.nextBytes(key2);
+
+		BloomFilterGenerator generatorLong = BloomFilterGenerator
+				.withHmacSha2HmacSha3BiGramHasher(BIT_SET_LENGTH, key1, key2, bcProvider);
+		BloomFilterGenerator generatorMedium = BloomFilterGenerator
+				.withHmacSha2HmacSha3BiGramHasher(BIT_SET_LENGTH_MEDIUM, key1, key2, bcProvider);
+		BloomFilterGenerator generatorShort = BloomFilterGenerator
+				.withHmacSha2HmacSha3BiGramHasher(BIT_SET_LENGTH_SHORT, key1, key2, bcProvider);
+
+		testRuntime(generatorLong, generatorMedium, generatorShort);
+		logTimes("SHA2 HMAC", "SHA3 HMAC");
+	}
+
+	private void testRuntime(BloomFilterGenerator generatorLong, BloomFilterGenerator generatorMedium,
+			BloomFilterGenerator generatorShort)
+	{
+		LocalDateTime starttime = LocalDateTime.now();
+
+		List<TestPerson> org1Pts = new ArrayList<>();
+		List<TestPerson> org2Pts = new ArrayList<>();
+		List<Double> weightList = Arrays
+				.asList(0.1, 0.1, 0.2, 0.1, 0.1, 0.05, 0.2, 0.1, 0.05); // Best weights for SHA23Hmac, MD5SHA1
 
 		for (IdatImpl r : org1)
 		{
@@ -179,29 +189,16 @@ public class MatchingTimeTest
 		Duration rlduration = Duration.between(rbftime, endtime);
 		rltimes.add(rlduration);
 
-		//		//		System.out.println(
-		//		//				"matched [" + org1Pts.size() + "] patients in " + Duration.between(t0, t1).toMillis() + "ms into "
-		//		//						+ matchPatients.size() + " patients");
-		//		List<Integer> confusionMt = calculateConfusionMatrixWithTrueNeg(matchPatients);
-		//		//		System.out.println(
-		//		//				"Confusion Matrix: \nTP: " + confusionMt.get(0) + "\nFP: " + confusionMt.get(1) + "\nFN: " + confusionMt
-		//		//						.get(2) + "\nTN: " + confusionMt.get(3));
-		//		List<Double> measures = calculateMeasures(confusionMt);
-		//		//		System.out.println(
-		//		//				"Measures: \nPrecision: " + measures.get(0) + "\nRecall: " + measures.get(1) + "\nAccuracy: " + measures
-		//		//						.get(2) + "\nF1: " + measures.get(3));
-		//
-		//		double tp = confusionMt.get(0);
-		//		double fp = confusionMt.get(1);
-		//		double fn = confusionMt.get(2);
-		//		double tn = confusionMt.get(3);
-		//		List<Double> resultsList = Arrays.asList(tp,fp,fn,tn,
-		//				measures.get(0),measures.get(1),measures.get(2),measures.get(3));
-		//		results.put(weightList, resultsList);
-
 		org1Pts = null;
 		org2Pts = null;
 		matchPatients = null;
+	}
+
+	private void logTimes(String hash1, String hash2)
+	{
+		logger.debug("Measured times for Hashing Combination " + hash1 + " + " + hash2 + "\n\n");
+		logger.debug("Avg Duration BF Gen: " + calculateAvgDuration(rbftimes) + " ms.");
+		logger.debug("Avg Duration Linkage: " + calculateAvgDuration(rltimes) + " ms.");
 	}
 
 	private List<FieldBloomFilter> createFbfList(IdatImpl r, List<Double> weightList,
@@ -248,7 +245,8 @@ public class MatchingTimeTest
 	{
 		if (durations.size() == 70)
 		{
-			for (int i = 0; i < 20; i++) {
+			for (int i = 0; i < 20; i++)
+			{
 				durations.remove(0); //Remove first 20 runs to disregard "warmup" times
 			}
 		}
@@ -323,7 +321,8 @@ public class MatchingTimeTest
 				}
 				if (Integer.parseInt(idNr) < 4000)
 				{
-					idats.add(new IdatImpl(r.get("rec_id"), r.get("given_name"), r.get("surname"), ptSex, bday, r.get("postcode"), r.get("address_2"), r.get("address_1"), r.get("state"),
+					idats.add(new IdatImpl(r.get("rec_id"), r.get("given_name"), r.get("surname"), ptSex, bday,
+							r.get("postcode"), r.get("address_2"), r.get("address_1"), r.get("state"),
 							r.get("soc_sec_id")));
 				}
 			}
