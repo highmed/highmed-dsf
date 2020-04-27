38fea68e753fa7217c4f7a573d75477ebf12bfea
Chris Heidt <christian.heidt@hs-heilbronn.de>
2020-04-28 00:28:09 +0200

    Implement Test for ResultSet Encoder TTP -> Medic Correct PSN/MEDIC Mixup in TTP ResultSet generation

---
+++ dsf-pseudonymization/dsf-pseudonymization-medic/src/test/java/org/highmed/pseudonymization/translation/ResultSetTranslatorFromTtpTest.java
@@ -0,0 +1,61 @@
+package org.highmed.pseudonymization.translation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.crypto.SecretKey;
+
+import org.highmed.openehr.json.OpenEhrObjectMapperFactory;
+import org.highmed.openehr.model.structure.ResultSet;
+import org.highmed.pseudonymization.crypto.AesGcmUtil;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ResultSetTranslatorFromTtpTest
+{
+	private static final Logger logger = LoggerFactory.getLogger(ResultSetTranslatorFromTtpTest.class);
+
+	@Test
+	public void testTranslateFromTtp() throws Exception
+	{
+		String researchStudyIdentifier = "researchStudy1";
+		SecretKey researchStudyKey = AesGcmUtil.generateAES256Key();
+		ObjectMapper openEhrObjectMapper = OpenEhrObjectMapperFactory.createObjectMapper();
+
+		ResultSetTranslatorFromTtp translator = new ResultSetTranslatorFromTtp(researchStudyIdentifier,
+				researchStudyKey, openEhrObjectMapper);
+		ResultSet resultSet = openEhrObjectMapper
+				.readValue(Files.readAllBytes(Paths.get("src/test/resources/result_5_PSN.json")), ResultSet.class);
+
+		assertNotNull(resultSet);
+		assertNotNull(resultSet.getColumns());
+		assertEquals(4, resultSet.getColumns().size());
+		assertNotNull(resultSet.getRows());
+		assertEquals(1, resultSet.getRows().size());
+		assertNotNull(resultSet.getRow(0));
+		assertEquals(4, resultSet.getRow(0).size());
+
+		ResultSet translatedResultSet = translator.translate(resultSet);
+		assertNotNull(translatedResultSet);
+		assertNotNull(translatedResultSet.getColumns());
+		assertEquals(4, translatedResultSet.getColumns().size());
+		assertNotNull(translatedResultSet.getRows());
+		assertEquals(1, translatedResultSet.getRows().size());
+		assertNotNull(translatedResultSet.getRow(0));
+		assertEquals(4, translatedResultSet.getRow(0).size());
+
+		DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+		prettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
+
+		logger.debug("Encoded ResultSet {}",
+				openEhrObjectMapper.writer(prettyPrinter).writeValueAsString(translatedResultSet));
+	}
+}
