540a61624ac1bfff2533466ab4e993001b45920b
Hauke Hund <hauke.hund@hs-heilbronn.de>
2020-05-26 18:26:14 +0200

    fix for process name change updateWhiteList -> updateWhitelist

--- dsf-bpe/dsf-bpe-process-update-whitelist/src/test/java/org/highmed/dsf/bpe/start/UpdateWhitelist3MedicTtpExampleStarter.java
+++ dsf-bpe/dsf-bpe-process-update-whitelist/src/test/java/org/highmed/dsf/bpe/start/UpdateWhitelist3MedicTtpExampleStarter.java
@@ -39,7 +39,7 @@ public class UpdateWhitelist3MedicTtpExampleStarter
 
 		Task task = new Task();
 		task.getMeta().addProfile("http://highmed.org/fhir/StructureDefinition/highmed-task-update-whitelist");
-		task.setInstantiatesUri("http://highmed.org/bpe/Process/updateWhiteList/0.2.0");
+		task.setInstantiatesUri("http://highmed.org/bpe/Process/updateWhitelist/0.2.0");
 		task.setStatus(TaskStatus.REQUESTED);
 		task.setIntent(TaskIntent.ORDER);
 		task.setAuthoredOn(new Date());
