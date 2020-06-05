fa11b322d31c9e9f2e1737a02903a1e9b237de40
Hauke Hund <hauke.hund@hs-heilbronn.de>
2020-06-05 23:56:28 +0200

    HAPI FHIR version upgrade 5.0.1 -> 5.0.2, Group Profile Test
    also added existing profile tests to TestSuiteProfileTests

--- dsf-fhir/dsf-fhir-server/src/test/java/org/highmed/dsf/fhir/test/TestSuiteProfileTests.java
+++ dsf-fhir/dsf-fhir-server/src/test/java/org/highmed/dsf/fhir/test/TestSuiteProfileTests.java
@@ -1,13 +1,17 @@
 package org.highmed.dsf.fhir.test;
 
+import org.highmed.dsf.fhir.profiles.EndpointProfileTest;
+import org.highmed.dsf.fhir.profiles.GroupProfileTest;
 import org.highmed.dsf.fhir.profiles.OrganizationProfileTest;
+import org.highmed.dsf.fhir.profiles.ResearchStudyProfileTest;
 import org.highmed.dsf.fhir.profiles.TaskProfileTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ OrganizationProfileTest.class, TaskProfileTest.class })
+@SuiteClasses({ EndpointProfileTest.class, GroupProfileTest.class, OrganizationProfileTest.class,
+		ResearchStudyProfileTest.class, TaskProfileTest.class })
 public class TestSuiteProfileTests
 {
 }
