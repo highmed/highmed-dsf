ba8acd74082db4ed525d93771a6c643ec4c7b1c9
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2020-06-24 15:59:58 +0200

    Adds log entries for selected mpi factory

--- dsf-bpe/dsf-bpe-server/src/main/java/org/highmed/dsf/bpe/spring/config/MasterPatientIndexConfig.java
+++ dsf-bpe/dsf-bpe-server/src/main/java/org/highmed/dsf/bpe/spring/config/MasterPatientIndexConfig.java
@@ -4,6 +4,9 @@ import java.util.NoSuchElementException;
 
 import org.highmed.mpi.client.MasterPatientIndexClientFactory;
 import org.highmed.mpi.client.MasterPatientIndexClientServiceLoader;
+import org.highmed.mpi.client.stub.MasterPatientIndexClientStubFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MasterPatientIndexConfig
 {
+	private static final Logger logger = LoggerFactory.getLogger(MasterPatientIndexConfig.class);
+
 	@Value("${org.highmed.dsf.bpe.mpi.webservice.factory.class:org.highmed.mpi.client.stub.MasterPatientIndexClientStubFactory}")
 	private String masterPatientIndexClientFactoryClass;
 
@@ -23,9 +28,16 @@ public class MasterPatientIndexConfig
 	@Bean
 	public MasterPatientIndexClientFactory masterPatientIndexClientFactory()
 	{
-		return masterPatientIndexClientServiceLoader()
+		MasterPatientIndexClientFactory factory = masterPatientIndexClientServiceLoader()
 				.getMasterPatientIndexClientFactory(masterPatientIndexClientFactoryClass)
 				.orElseThrow(() -> new NoSuchElementException("Master patient index client factory with classname='"
-						+ masterPatientIndexClientFactoryClass+ "' not found"));
+								+ masterPatientIndexClientFactoryClass + "' not found"));
+
+		if(factory instanceof MasterPatientIndexClientStubFactory)
+			logger.warn("Using {} as MPI client factory", factory.getClass().getName());
+		else
+			logger.info("Using {} as MPI client factory", factory.getClass().getName());
+
+		return factory;
 	}
 }
