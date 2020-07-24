ce129f451674e86a8f60cf81e69edeb9c813d1b9
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2020-07-24 15:22:00 +0200

    removes duplicated object serializer

--- dsf-bpe/dsf-bpe-process-feasibility/src/main/java/org/highmed/dsf/bpe/spring/config/FeasibilitySerializerConfig.java
+++ dsf-bpe/dsf-bpe-process-feasibility/src/main/java/org/highmed/dsf/bpe/spring/config/FeasibilitySerializerConfig.java
@@ -7,17 +7,11 @@ import org.highmed.dsf.bpe.variables.FeasibilityQueryResultSerializer;
 import org.highmed.dsf.bpe.variables.FeasibilityQueryResultsSerializer;
 import org.highmed.dsf.bpe.variables.FinalFeasibilityQueryResultSerializer;
 import org.highmed.dsf.bpe.variables.FinalFeasibilityQueryResultsSerializer;
-import org.highmed.dsf.fhir.variables.FhirResourceJacksonDeserializer;
-import org.highmed.dsf.fhir.variables.FhirResourceJacksonSerializer;
-import org.highmed.openehr.json.OpenEhrObjectMapperFactory;
-import org.hl7.fhir.r4.model.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import ca.uhn.fhir.context.FhirContext;
 
@@ -27,52 +21,37 @@ public class FeasibilitySerializerConfig
 	@Autowired
 	FhirContext fhirContext;
 
-	@Bean
-	public ObjectMapper objectMapper()
-	{
-		ObjectMapper mapper = new ObjectMapper();
-
-		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-
-		SimpleModule module = new SimpleModule();
-		module.addSerializer(Resource.class, new FhirResourceJacksonSerializer(fhirContext));
-		module.addDeserializer(Resource.class, new FhirResourceJacksonDeserializer(fhirContext));
-
-		mapper.registerModule(module);
-		mapper.registerModule(OpenEhrObjectMapperFactory.openEhrModule());
-
-		return mapper;
-	}
+	@Autowired
+	public ObjectMapper objectMapper;
 
 	@Bean
 	public FeasibilityQueryResultSerializer feasibilityQueryResultSerializer()
 	{
-		return new FeasibilityQueryResultSerializer(objectMapper());
+		return new FeasibilityQueryResultSerializer(objectMapper);
 	}
 
 	@Bean
 	public FeasibilityQueryResultsSerializer feasibilityQueryResultsSerializer()
 	{
-		return new FeasibilityQueryResultsSerializer(objectMapper());
+		return new FeasibilityQueryResultsSerializer(objectMapper);
 	}
 
 	@Bean
 	public FinalFeasibilityQueryResultSerializer finalFeasibilityQueryResultSerializer()
 	{
-		return new FinalFeasibilityQueryResultSerializer(objectMapper());
+		return new FinalFeasibilityQueryResultSerializer(objectMapper);
 	}
 
 	@Bean
 	public FinalFeasibilityQueryResultsSerializer finalFeasibilityQueryResultsSerializer()
 	{
-		return new FinalFeasibilityQueryResultsSerializer(objectMapper());
+		return new FinalFeasibilityQueryResultsSerializer(objectMapper);
 	}
 
 	@Bean
 	public BloomFilterConfigSerializer bloomFilterConfigSerializer()
 	{
-		return new BloomFilterConfigSerializer(objectMapper());
+		return new BloomFilterConfigSerializer(objectMapper);
 	}
 
 	@Bean
