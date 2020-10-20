17372e01fa2da31713b379e5d53670341eef2271
Hauke Hund <hauke.hund@hs-heilbronn.de>
2020-10-21 01:53:30 +0200

    restructuring an retry implementation

---
+++ dsf-fhir/dsf-fhir-webservice-client/src/main/java/org/highmed/fhir/client/PreferReturnMinimalWithRetry.java
@@ -0,0 +1,5 @@
+package org.highmed.fhir.client;
+
+public interface PreferReturnMinimalWithRetry extends PreferReturnMinimal, RetryClient<PreferReturnMinimal>
+{
+}
---
+++ dsf-fhir/dsf-fhir-webservice-client/src/main/java/org/highmed/fhir/client/PreferReturnOutcomeWithRetry.java
@@ -0,0 +1,5 @@
+package org.highmed.fhir.client;
+
+public interface PreferReturnOutcomeWithRetry extends PreferReturnOutcome, RetryClient<PreferReturnOutcome>
+{
+}
