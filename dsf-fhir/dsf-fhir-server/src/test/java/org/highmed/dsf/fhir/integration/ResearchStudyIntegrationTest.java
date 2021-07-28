e7a9f72c0907b0ac545bf956fad7ba22f2668c54
Hauke Hund <hauke.hund@hs-heilbronn.de>
2021-07-28 14:12:51 +0200

    new and improved tests for rel. artifacts and ref. check/resolution

--- dsf-fhir/dsf-fhir-server/src/test/java/org/highmed/dsf/fhir/integration/ResearchStudyIntegrationTest.java
+++ dsf-fhir/dsf-fhir-server/src/test/java/org/highmed/dsf/fhir/integration/ResearchStudyIntegrationTest.java
@@ -1,9 +1,6 @@
 package org.highmed.dsf.fhir.integration;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.hl7.fhir.r4.model.Bundle.BundleType.TRANSACTION;
-import static org.hl7.fhir.r4.model.RelatedArtifact.RelatedArtifactType.DOCUMENTATION;
-import static org.hl7.fhir.r4.model.ResearchStudy.ResearchStudyStatus.ACTIVE;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -15,19 +12,22 @@ import org.highmed.dsf.fhir.dao.GroupDao;
 import org.highmed.dsf.fhir.dao.ResearchStudyDao;
 import org.hl7.fhir.r4.model.Binary;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleType;
+import org.hl7.fhir.r4.model.Bundle.HTTPVerb;
 import org.hl7.fhir.r4.model.CodeType;
 import org.hl7.fhir.r4.model.Group;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.RelatedArtifact.RelatedArtifactType;
 import org.hl7.fhir.r4.model.ResearchStudy;
-import org.hl7.fhir.r4.model.ResourceType;
+import org.hl7.fhir.r4.model.ResearchStudy.ResearchStudyStatus;
 import org.junit.Test;
 
 public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 {
 	@Test
-	public void testSearchResearchStudyByGroupId() throws Exception
+	public void testSearchByGroupId() throws Exception
 	{
 		Group g = new Group();
 		readAccessHelper.addLocal(g);
@@ -51,7 +51,7 @@ public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 	}
 
 	@Test
-	public void testResearchStudyRelatedArtefactUnknownUrl() throws Exception
+	public void testCreateWithRelatedArtefactUnknownUrl() throws Exception
 	{
 		String url = "https://foo.bar";
 		ResearchStudy researchStudy = getResearchStudy(url);
@@ -67,25 +67,57 @@ public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 	}
 
 	@Test
-	public void testResearchStudyRelatedArtefactTemporaryUrlResolution() throws Exception
+	public void testCreateViaBundleWithRelatedArtefactUnknownUrl() throws Exception
+	{
+		String url = "https://foo.bar";
+		ResearchStudy researchStudy = getResearchStudy(url);
+
+		Bundle bundle = new Bundle().setType(BundleType.TRANSACTION);
+		bundle.addEntry().setFullUrl("urn:uuid:" + UUID.randomUUID().toString()).setResource(researchStudy).getRequest()
+				.setUrl("ResearchStudy").setMethod(HTTPVerb.POST);
+
+		Bundle resultBundle = getWebserviceClient().postBundle(bundle);
+
+		assertNotNull(resultBundle);
+		assertEquals(1, resultBundle.getEntry().size());
+		assertNotNull(resultBundle.getEntry().get(0));
+		assertNotNull(resultBundle.getEntry().get(0).getResource());
+		assertTrue(resultBundle.getEntry().get(0).getResource() instanceof ResearchStudy);
+
+		ResearchStudy researchStudyResult = (ResearchStudy) resultBundle.getEntry().get(0).getResource();
+
+		assertNotNull(researchStudyResult);
+		assertEquals(1, researchStudyResult.getRelatedArtifact().size());
+
+		String relatedArtifactResultUrl = researchStudyResult.getRelatedArtifact().get(0).getUrl();
+
+		assertEquals(url, relatedArtifactResultUrl);
+	}
+
+	@Test
+	public void testCreateViaBundleWithRelatedArtefactTemporaryUrl() throws Exception
 	{
 		String binaryUrl = "urn:uuid:" + UUID.randomUUID().toString();
 		Binary binary = getBinary();
 
 		ResearchStudy researchStudy = getResearchStudy(binaryUrl);
 
-		Bundle bundle = new Bundle().setType(TRANSACTION);
+		Bundle bundle = new Bundle().setType(BundleType.TRANSACTION);
 		bundle.addEntry().setFullUrl("urn:uuid:" + UUID.randomUUID().toString()).setResource(researchStudy).getRequest()
-				.setUrl("ResearchStudy").setMethod(Bundle.HTTPVerb.POST);
+				.setUrl("ResearchStudy").setMethod(HTTPVerb.POST);
 		bundle.addEntry().setFullUrl(binaryUrl).setResource(binary).getRequest().setUrl("Binary")
-				.setMethod(Bundle.HTTPVerb.POST);
+				.setMethod(HTTPVerb.POST);
 
 		Bundle resultBundle = getWebserviceClient().postBundle(bundle);
 
 		assertNotNull(resultBundle);
 		assertEquals(2, resultBundle.getEntry().size());
-		assertEquals(ResourceType.ResearchStudy, resultBundle.getEntry().get(0).getResource().getResourceType());
-		assertEquals(ResourceType.Binary, resultBundle.getEntry().get(1).getResource().getResourceType());
+		assertNotNull(resultBundle.getEntry().get(0));
+		assertNotNull(resultBundle.getEntry().get(0).getResource());
+		assertTrue(resultBundle.getEntry().get(0).getResource() instanceof ResearchStudy);
+		assertNotNull(resultBundle.getEntry().get(1));
+		assertNotNull(resultBundle.getEntry().get(1).getResource());
+		assertTrue(resultBundle.getEntry().get(1).getResource() instanceof Binary);
 
 		ResearchStudy researchStudyResult = (ResearchStudy) resultBundle.getEntry().get(0).getResource();
 		Binary binaryResult = (Binary) resultBundle.getEntry().get(1).getResource();
@@ -94,15 +126,15 @@ public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 
 		String relatedArtifactResultUrl = researchStudyResult.getRelatedArtifact().get(0).getUrl();
 		String binaryResultUrl = binaryResult.getIdElement()
-				.withServerBase(getWebserviceClient().getBaseUrl(), ResourceType.Binary.name()).toVersionless()
-				.getValue();
+				.withServerBase(getWebserviceClient().getBaseUrl(), binaryResult.getResourceType().name())
+				.toVersionless().getValue();
 
-		assertTrue(new IdType(relatedArtifactResultUrl).isAbsolute());
 		assertEquals(binaryResultUrl, relatedArtifactResultUrl);
+		assertTrue(new IdType(relatedArtifactResultUrl).isAbsolute());
 	}
 
 	@Test
-	public void testResearchStudyRelatedArtefactConditionalUrlResolution() throws Exception
+	public void testCreateViaBundleWithRelatedArtefactConditionalUrl() throws Exception
 	{
 		Binary binary = getBinary();
 		Binary binaryResult = getWebserviceClient().create(binary);
@@ -111,15 +143,17 @@ public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 
 		ResearchStudy researchStudy = getResearchStudy("Binary?_id=" + binaryResult.getIdElement().getIdPart());
 
-		Bundle bundle = new Bundle().setType(TRANSACTION);
+		Bundle bundle = new Bundle().setType(BundleType.TRANSACTION);
 		bundle.addEntry().setFullUrl("urn:uuid:" + UUID.randomUUID().toString()).setResource(researchStudy).getRequest()
-				.setUrl("ResearchStudy").setMethod(Bundle.HTTPVerb.POST);
+				.setUrl("ResearchStudy").setMethod(HTTPVerb.POST);
 
 		Bundle resultBundle = getWebserviceClient().postBundle(bundle);
 
 		assertNotNull(resultBundle);
 		assertEquals(1, resultBundle.getEntry().size());
-		assertEquals(ResourceType.ResearchStudy, resultBundle.getEntry().get(0).getResource().getResourceType());
+		assertNotNull(resultBundle.getEntry().get(0));
+		assertNotNull(resultBundle.getEntry().get(0).getResource());
+		assertTrue(resultBundle.getEntry().get(0).getResource() instanceof ResearchStudy);
 
 		ResearchStudy researchStudyResult = (ResearchStudy) resultBundle.getEntry().get(0).getResource();
 
@@ -127,15 +161,15 @@ public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 
 		String relatedArtifactResultUrl = researchStudyResult.getRelatedArtifact().get(0).getUrl();
 		String binaryResultUrl = binaryResult.getIdElement()
-				.withServerBase(getWebserviceClient().getBaseUrl(), ResourceType.Binary.name()).toVersionless()
-				.getValue();
+				.withServerBase(getWebserviceClient().getBaseUrl(), binaryResult.getResourceType().name())
+				.toVersionless().getValue();
 
-		assertTrue(new IdType(relatedArtifactResultUrl).isAbsolute());
 		assertEquals(binaryResultUrl, relatedArtifactResultUrl);
+		assertTrue(new IdType(relatedArtifactResultUrl).isAbsolute());
 	}
 
 	@Test
-	public void testResearchStudyRelatedArtefactLiteralInternalUrlResolution() throws Exception
+	public void testCreateViaBundleWithRelatedArtefactRelativeLiteralInternalUrl() throws Exception
 	{
 		Binary binary = getBinary();
 		Binary binaryResult = getWebserviceClient().create(binary);
@@ -144,31 +178,33 @@ public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 
 		ResearchStudy researchStudy = getResearchStudy(
 				binaryResult.getIdElement().toUnqualifiedVersionless().getValue());
-		Bundle bundle = new Bundle().setType(TRANSACTION);
+		Bundle bundle = new Bundle().setType(BundleType.TRANSACTION);
 		bundle.addEntry().setFullUrl("urn:uuid:" + UUID.randomUUID().toString()).setResource(researchStudy).getRequest()
-				.setUrl("ResearchStudy").setMethod(Bundle.HTTPVerb.POST);
+				.setUrl("ResearchStudy").setMethod(HTTPVerb.POST);
 
 		Bundle resultBundle = getWebserviceClient().postBundle(bundle);
 
 		assertNotNull(resultBundle);
 		assertEquals(1, resultBundle.getEntry().size());
-		assertEquals(ResourceType.ResearchStudy, resultBundle.getEntry().get(0).getResource().getResourceType());
+		assertNotNull(resultBundle.getEntry().get(0));
+		assertNotNull(resultBundle.getEntry().get(0).getResource());
+		assertTrue(resultBundle.getEntry().get(0).getResource() instanceof ResearchStudy);
 
 		ResearchStudy researchStudyResult = (ResearchStudy) resultBundle.getEntry().get(0).getResource();
 
 		assertEquals(1, researchStudyResult.getRelatedArtifact().size());
 
 		String relatedArtifactResultUrl = researchStudyResult.getRelatedArtifact().get(0).getUrl();
-		String binaryResultUrl = binaryResult.getIdElement()
-				.withServerBase(getWebserviceClient().getBaseUrl(), ResourceType.Binary.name()).toVersionless()
-				.getValue();
 
-		assertTrue(new IdType(relatedArtifactResultUrl).isAbsolute());
+		String binaryResultUrl = binaryResult.getIdElement()
+				.withServerBase(getWebserviceClient().getBaseUrl(), binaryResult.getResourceType().name())
+				.toVersionless().getValue();
 		assertEquals(binaryResultUrl, relatedArtifactResultUrl);
+		assertTrue(new IdType(relatedArtifactResultUrl).isAbsolute());
 	}
 
 	@Test
-	public void testResearchStudyRelatedArtefactLiteralInternalUrlResolution2() throws Exception
+	public void testCreateViaBundleWithRelatedArtefactAbsoluteLiteralInternalUrl() throws Exception
 	{
 		Binary binary = getBinary();
 		Binary binaryResult = getWebserviceClient().create(binary);
@@ -176,23 +212,94 @@ public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 		assertNotNull(binaryResult);
 
 		String binaryResultUrl = binaryResult.getIdElement()
-				.withServerBase(getWebserviceClient().getBaseUrl(), ResourceType.Binary.name()).toVersionless()
-				.getValue();
+				.withServerBase(getWebserviceClient().getBaseUrl(), binaryResult.getResourceType().name())
+				.toVersionless().getValue();
 
 		ResearchStudy researchStudy = getResearchStudy(binaryResultUrl);
 
-		Bundle bundle = new Bundle().setType(TRANSACTION);
+		Bundle bundle = new Bundle().setType(BundleType.TRANSACTION);
 		bundle.addEntry().setFullUrl("urn:uuid:" + UUID.randomUUID().toString()).setResource(researchStudy).getRequest()
-				.setUrl("ResearchStudy").setMethod(Bundle.HTTPVerb.POST);
+				.setUrl("ResearchStudy").setMethod(HTTPVerb.POST);
 
 		Bundle resultBundle = getWebserviceClient().postBundle(bundle);
 
 		assertNotNull(resultBundle);
 		assertEquals(1, resultBundle.getEntry().size());
-		assertEquals(ResourceType.ResearchStudy, resultBundle.getEntry().get(0).getResource().getResourceType());
+		assertNotNull(resultBundle.getEntry().get(0));
+		assertNotNull(resultBundle.getEntry().get(0).getResource());
+		assertTrue(resultBundle.getEntry().get(0).getResource() instanceof ResearchStudy);
 
 		ResearchStudy researchStudyResult = (ResearchStudy) resultBundle.getEntry().get(0).getResource();
 
+		assertEquals(1, researchStudyResult.getRelatedArtifact().size());
+
+		String relatedArtifactResultUrl = researchStudyResult.getRelatedArtifact().get(0).getUrl();
+
+		assertEquals(binaryResultUrl, relatedArtifactResultUrl);
+		assertTrue(new IdType(relatedArtifactResultUrl).isAbsolute());
+	}
+
+	@Test
+	public void testCreateWithRelatedArtefactRelativeLiteralInternalUrl() throws Exception
+	{
+		Binary binary = getBinary();
+		Binary binaryResult = getWebserviceClient().create(binary);
+
+		assertNotNull(binaryResult);
+
+		ResearchStudy researchStudy = getResearchStudy(
+				binaryResult.getIdElement().toUnqualifiedVersionless().getValue());
+		ResearchStudy researchStudyResult = getWebserviceClient().create(researchStudy);
+
+		assertNotNull(researchStudyResult);
+		assertEquals(1, researchStudyResult.getRelatedArtifact().size());
+
+		String relatedArtifactResultUrl = researchStudyResult.getRelatedArtifact().get(0).getUrl();
+		String binaryResultUrl = binaryResult.getIdElement()
+				.withServerBase(getWebserviceClient().getBaseUrl(), binaryResult.getResourceType().name())
+				.toVersionless().getValue();
+
+		assertEquals(binaryResultUrl, relatedArtifactResultUrl);
+		assertTrue(new IdType(relatedArtifactResultUrl).isAbsolute());
+	}
+
+	@Test
+	public void testCreateWithRelatedArtefactRelativeLiteralInternalUrlNonExisting() throws Exception
+	{
+		ResearchStudy researchStudy = getResearchStudy("Binary/" + UUID.randomUUID().toString());
+
+		expectForbidden(() -> getWebserviceClient().create(researchStudy));
+	}
+
+	@Test
+	public void testCreateViaBundleWithRelatedArtefactRelativeLiteralInternalUrlNonExisting()
+			throws Exception
+	{
+		ResearchStudy researchStudy = getResearchStudy("Binary/" + UUID.randomUUID().toString());
+
+		Bundle bundle = new Bundle().setType(BundleType.TRANSACTION);
+		bundle.addEntry().setFullUrl("urn:uuid:" + UUID.randomUUID().toString()).setResource(researchStudy).getRequest()
+				.setUrl("ResearchStudy").setMethod(HTTPVerb.POST);
+
+		expectForbidden(() -> getWebserviceClient().postBundle(bundle));
+	}
+
+	@Test
+	public void testCreateWithRelatedArtefactAbsoluteLiteralInternalUrl() throws Exception
+	{
+		Binary binary = getBinary();
+		Binary binaryResult = getWebserviceClient().create(binary);
+
+		assertNotNull(binaryResult);
+
+		String binaryResultUrl = binaryResult.getIdElement()
+				.withServerBase(getWebserviceClient().getBaseUrl(), binaryResult.getResourceType().name())
+				.toVersionless().getValue();
+
+		ResearchStudy researchStudy = getResearchStudy(binaryResultUrl);
+		ResearchStudy researchStudyResult = getWebserviceClient().create(researchStudy);
+
+		assertNotNull(researchStudyResult);
 		assertEquals(1, researchStudyResult.getRelatedArtifact().size());
 
 		String relatedArtifactResultUrl = researchStudyResult.getRelatedArtifact().get(0).getUrl();
@@ -212,10 +319,10 @@ public class ResearchStudyIntegrationTest extends AbstractIntegrationTest
 	private ResearchStudy getResearchStudy(String url)
 	{
 		ResearchStudy researchStudy = new ResearchStudy();
-		researchStudy.setStatus(ACTIVE);
+		researchStudy.setStatus(ResearchStudyStatus.ACTIVE);
 		researchStudy.addIdentifier().setSystem("http://highmed.org/sid/research-study-identifier")
 				.setValue(UUID.randomUUID().toString());
-		researchStudy.addRelatedArtifact().setType(DOCUMENTATION).setUrl(url);
+		researchStudy.addRelatedArtifact().setType(RelatedArtifactType.DOCUMENTATION).setUrl(url);
 		researchStudy.addExtension().setUrl("http://highmed.org/fhir/StructureDefinition/extension-participating-ttp")
 				.setValue(new Reference().setType("Organization").setIdentifier(new Identifier()
 						.setSystem("http://highmed.org/sid/organization-identifier").setValue("Test_Organization")));
