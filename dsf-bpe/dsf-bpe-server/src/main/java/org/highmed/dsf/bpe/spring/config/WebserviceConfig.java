7bab0c0c1de3b3b3f3ab0084879d04ecc3317962
Hauke Hund <hauke.hund@hs-heilbronn.de>
2021-11-30 16:36:30 +0100

    bpe status webservice accessible via http://127.0.0.1:10002/bpe/status

--- dsf-bpe/dsf-bpe-server/src/main/java/org/highmed/dsf/bpe/spring/config/WebserviceConfig.java
+++ dsf-bpe/dsf-bpe-server/src/main/java/org/highmed/dsf/bpe/spring/config/WebserviceConfig.java
@@ -2,6 +2,7 @@ package org.highmed.dsf.bpe.spring.config;
 
 import org.camunda.bpm.engine.ProcessEngine;
 import org.highmed.dsf.bpe.webservice.ProcessService;
+import org.highmed.dsf.bpe.webservice.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,18 @@ public class WebserviceConfig
 	@Autowired
 	private ProcessEngine processEngine;
 
+	@Autowired
+	private DaoConfig daoConfig;
+
 	@Bean
 	public ProcessService processService()
 	{
 		return new ProcessService(processEngine.getRuntimeService(), processEngine.getRepositoryService());
+	}
+
+	@Bean
+	public StatusService statusService()
+	{
+		return new StatusService(daoConfig.dataSource());
 	}
 }
