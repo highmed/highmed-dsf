ab4a3a0e08c98017e22afbb44fb47bd23ad7d9f7
Hauke Hund <hauke.hund@hs-heilbronn.de>
2022-04-26 15:36:47 +0200

    fixed and simplified regex
    Regex of help.js was capturing OrganizationAffiliation URLs as
    Organization URLs.
    Switch to non-capturing groups (?:) where applicable.

--- dsf-fhir/dsf-fhir-server/src/main/resources/static/help.js
+++ dsf-fhir/dsf-fhir-server/src/main/resources/static/help.js
@@ -33,25 +33,25 @@ function createAndShowHelp(httpRequest) {
 				}
 			}
 			else {
-				const searchParam = metadata.rest[0].resource.filter(r => r.type === resourceType[0])[0].searchParam;
+				const searchParam = metadata.rest[0].resource.filter(r => r.type === resourceType[1])[0].searchParam;
 				//Resource
-				if (resourceType[0] !== undefined && resourceType[1] === undefined && resourceType[2] === undefined && resourceType[3] === undefined) {
+				if (resourceType[1] !== undefined && resourceType[2] === undefined && resourceType[3] === undefined && resourceType[4] === undefined) {
 					createHelp(searchParam);
 				}
 				//Resource/_history
-				else if (resourceType[0] !== undefined && resourceType[1] === undefined && resourceType[2] !== undefined && resourceType[3] === undefined) {
+				else if (resourceType[1] !== undefined && resourceType[2] === undefined && resourceType[3] !== undefined && resourceType[4] === undefined) {
 					createHelp(searchParam.filter(p => ['_count', '_format', '_page', '_pretty'].includes(p.name)));
 				}
 				//Resource/id
-				else if (resourceType[0] !== undefined && resourceType[1] !== undefined && resourceType[2] === undefined && resourceType[3] === undefined) {
+				else if (resourceType[1] !== undefined && resourceType[2] !== undefined && resourceType[3] === undefined && resourceType[4] === undefined) {
 					createHelp(searchParam.filter(p => ['_format', '_pretty'].includes(p.name)));
 				}
 				//Resource/id/_history
-				else if (resourceType[0] !== undefined && resourceType[1] !== undefined && resourceType[2] !== undefined && resourceType[3] === undefined) {
+				else if (resourceType[1] !== undefined && resourceType[2] !== undefined && resourceType[3] !== undefined && resourceType[4] === undefined) {
 					createHelp(searchParam.filter(p => ['_count', '_format', '_page', '_pretty'].includes(p.name)));
 				}
 				//Resource/id/_history/version
-				else if (resourceType[0] !== undefined && resourceType[1] !== undefined && resourceType[2] !== undefined && resourceType[3] !== undefined) {
+				else if (resourceType[1] !== undefined && resourceType[2] !== undefined && resourceType[3] !== undefined && resourceType[4] !== undefined) {
 					createHelp(searchParam.filter(p => ['_format', '_pretty'].includes(p.name)));
 				}
 			}
@@ -72,14 +72,10 @@ function createAndShowHelp(httpRequest) {
 
 function getResourceTypeForCurrentUrl() {
 	const url = window.location.pathname;
-	const regex = new RegExp('(([A-Za-z0-9\-\\\.\:\%\$]*\/)+)?'
+	const regex = new RegExp('^(?:(?:[A-Za-z0-9\-\\\.\:\%\$]*\/)+)?'
 		+ '(Account|ActivityDefinition|AdverseEvent|AllergyIntolerance|Appointment|AppointmentResponse|AuditEvent|Basic|Binary|BiologicallyDerivedProduct|BodyStructure|Bundle|CapabilityStatement|CarePlan|CareTeam|CatalogEntry|ChargeItem|ChargeItemDefinition|Claim|ClaimResponse|ClinicalImpression|CodeSystem|Communication|CommunicationRequest|CompartmentDefinition|Composition|ConceptMap|Condition|Consent|Contract|Coverage|CoverageEligibilityRequest|CoverageEligibilityResponse|DetectedIssue|Device|DeviceDefinition|DeviceMetric|DeviceRequest|DeviceUseStatement|DiagnosticReport|DocumentManifest|DocumentReference|EffectEvidenceSynthesis|Encounter|Endpoint|EnrollmentRequest|EnrollmentResponse|EpisodeOfCare|EventDefinition|Evidence|EvidenceVariable|ExampleScenario|ExplanationOfBenefit|FamilyMemberHistory|Flag|Goal|GraphDefinition|Group|GuidanceResponse|HealthcareService|ImagingStudy|Immunization|ImmunizationEvaluation|ImmunizationRecommendation|ImplementationGuide|InsurancePlan|Invoice|Library|Linkage|List|Location|Measure|MeasureReport|Media|Medication|MedicationAdministration|MedicationDispense|MedicationKnowledge|MedicationRequest|MedicationStatement|MedicinalProduct|MedicinalProductAuthorization|MedicinalProductContraindication|MedicinalProductIndication|MedicinalProductIngredient|MedicinalProductInteraction|MedicinalProductManufactured|MedicinalProductPackaged|MedicinalProductPharmaceutical|MedicinalProductUndesirableEffect|MessageDefinition|MessageHeader|MolecularSequence|NamingSystem|NutritionOrder|Observation|ObservationDefinition|OperationDefinition|OperationOutcome|Organization|OrganizationAffiliation|Patient|PaymentNotice|PaymentReconciliation|Person|PlanDefinition|Practitioner|PractitionerRole|Procedure|Provenance|Questionnaire|QuestionnaireResponse|RelatedPerson|RequestGroup|ResearchDefinition|ResearchElementDefinition|ResearchStudy|ResearchSubject|RiskAssessment|RiskEvidenceSynthesis|Schedule|SearchParameter|ServiceRequest|Slot|Specimen|SpecimenDefinition|StructureDefinition|StructureMap|Subscription|Substance|SubstanceNucleicAcid|SubstancePolymer|SubstanceProtein|SubstanceReferenceInformation|SubstanceSourceMaterial|SubstanceSpecification|SupplyDelivery|SupplyRequest|Task|TerminologyCapabilities|TestReport|TestScript|ValueSet|VerificationResult|VisionPrescription)'
-		+ '(\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})?(\/_history)?(\/[0-9]+)?.*');
-	const match = regex.exec(url);
-	if (match != null && match.length == 7)
-		return [match[3], match[4], match[5], match[6]];
-	else
-		return null;
+		+ '(\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})?(\/_history)?(\/[0-9]+)?(?:|\\?.*)$');
+	return regex.exec(url);
 }
 
 function createHelp(searchParam) {
