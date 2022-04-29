1ef9621453d98f516551c0d3a89ff1ed68849ec5
Hauke Hund <hauke.hund@hs-heilbronn.de>
2022-04-29 13:20:57 +0200

    added Endpoint identifier field
    deprecated factory methods without Endpoint identifier parameter

--- dsf-bpe/dsf-bpe-process-base/src/main/java/org/highmed/dsf/fhir/variables/Target.java
+++ dsf-bpe/dsf-bpe-process-base/src/main/java/org/highmed/dsf/fhir/variables/Target.java
@@ -1,44 +1,63 @@
 package org.highmed.dsf.fhir.variables;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Target
 {
-	private static final Logger logger = LoggerFactory.getLogger(Target.class);
-
 	private final String targetOrganizationIdentifierValue;
+	private final String targetEndpointIdentifierValue;
 	private final String targetEndpointUrl;
 	private final String correlationKey;
 
 	@JsonCreator
 	private Target(@JsonProperty("targetOrganizationIdentifierValue") String targetOrganizationIdentifierValue,
+			@JsonProperty("targetEndpointIdentifierValue") String targetEndpointIdentifierValue,
 			@JsonProperty("targetEndpointUrl") String targetEndpointUrl,
 			@JsonProperty("correlationKey") String correlationKey)
 	{
 		this.targetOrganizationIdentifierValue = targetOrganizationIdentifierValue;
+		this.targetEndpointIdentifierValue = targetEndpointIdentifierValue;
 		this.targetEndpointUrl = targetEndpointUrl;
 		this.correlationKey = correlationKey;
 	}
 
-	public static Target createUniDirectionalTarget(String targetOrganizationIdentifierValue, String targetEndpointUrl)
+	public static Target createUniDirectionalTarget(String targetOrganizationIdentifierValue,
+			String targetEndpointIdentifierValue, String targetEndpointUrl)
 	{
-		return new Target(targetOrganizationIdentifierValue, targetEndpointUrl, null);
+		return new Target(targetOrganizationIdentifierValue, targetEndpointIdentifierValue, targetEndpointUrl, null);
 	}
 
+	@Deprecated
+	public static Target createUniDirectionalTarget(String targetOrganizationIdentifierValue, String targetEndpointUrl)
+	{
+		return createUniDirectionalTarget(targetOrganizationIdentifierValue, null, targetEndpointUrl);
+	}
+
+	public static Target createBiDirectionalTarget(String targetOrganizationIdentifierValue,
+			String targetEndpointIdentifierValue, String targetEndpointUrl, String correlationKey)
+	{
+		return new Target(targetOrganizationIdentifierValue, targetEndpointIdentifierValue, targetEndpointUrl,
+				correlationKey);
+	}
+
+	@Deprecated
 	public static Target createBiDirectionalTarget(String targetOrganizationIdentifierValue, String targetEndpointUrl,
 			String correlationKey)
 	{
-		return new Target(targetOrganizationIdentifierValue, targetEndpointUrl, correlationKey);
+		return createBiDirectionalTarget(targetOrganizationIdentifierValue, null, targetEndpointUrl, correlationKey);
 	}
 
 	@JsonProperty("targetOrganizationIdentifierValue")
 	public String getTargetOrganizationIdentifierValue()
 	{
 		return targetOrganizationIdentifierValue;
+	}
+
+	@JsonProperty("targetEndpointIdentifierValue")
+	public String getTargetEndpointIdentifierValue()
+	{
+		return targetEndpointIdentifierValue;
 	}
 
 	@JsonProperty("targetEndpointUrl")
@@ -50,9 +69,6 @@ public class Target
 	@JsonProperty("correlationKey")
 	public String getCorrelationKey()
 	{
-		if (correlationKey == null)
-			logger.debug("SingleInstanceTargets do not have a correlation key");
-
 		return correlationKey;
 	}
 }
