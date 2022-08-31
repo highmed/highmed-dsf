fac398fcc87464d8ed4c8b21d2afcb125983cca6
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2022-08-31 11:28:12 +0200

    html form for all supported data types, form validaton

--- dsf-fhir/dsf-fhir-rest-adapter/src/main/java/org/highmed/dsf/fhir/adapter/QuestionnaireResponseHtmlFhirAdapter.java
+++ dsf-fhir/dsf-fhir-rest-adapter/src/main/java/org/highmed/dsf/fhir/adapter/QuestionnaireResponseHtmlFhirAdapter.java
@@ -2,6 +2,8 @@ package org.highmed.dsf.fhir.adapter;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.ws.rs.ext.Provider;
 
@@ -24,6 +26,10 @@ public class QuestionnaireResponseHtmlFhirAdapter extends HtmlFhirAdapter<Questi
 	private static final String CODESYSTEM_HIGHMED_BPMN_USER_TASK_VALUE_BUSINESS_KEY = "business-key";
 	private static final String CODESYSTEM_HIGHMED_BPMN_USER_TASK_VALUE_USER_TASK_ID = "user-task-id";
 
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+	private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+	private static final SimpleDateFormat DATE_TIME_DISPLAY_FORMAT = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+
 	public QuestionnaireResponseHtmlFhirAdapter(FhirContext fhirContext, ServerBaseProvider serverBaseProvider)
 	{
 		super(fhirContext, serverBaseProvider, QuestionnaireResponse.class);
@@ -44,29 +50,83 @@ public class QuestionnaireResponseHtmlFhirAdapter extends HtmlFhirAdapter<Questi
 	@Override
 	protected void doWriteHtml(QuestionnaireResponse questionnaireResponse, OutputStreamWriter out) throws IOException
 	{
-		out.write("<form>");
+		boolean isCompleted = QuestionnaireResponse.QuestionnaireResponseStatus.COMPLETED
+				.equals(questionnaireResponse.getStatus());
+		out.write("<div id=\"spinner\" class=\"spinner spinner-disabled\"></div>");
+		out.write("<form>\n");
+		out.write("<div class=\"row row-info " + (isCompleted ? "info-color-completed" : "info-color-progress")
+				+ "\">\n");
+
+		out.write("<div>");
+		out.write("<svg class=\"info-icon\" id=\"info-icon\" viewBox=\"0 0 24 24\">\n");
+		out.write("<title>Info</title>\n");
+		out.write("<path class=\"" + (isCompleted ? "info-path-completed" : "info-path-progress")
+				+ "\" d=\"M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-.001 5.75c.69 0 1.251.56 1.251 1.25s-.561 1.25-1.251 1.25-1.249-.56-1.249-1.25.559-1.25 1.249-1.25zm2.001 12.25h-4v-1c.484-.179 1-.201 1-.735v-4.467c0-.534-.516-.618-1-.797v-1h3v6.265c0 .535.517.558 1 .735v.999z\"/>\"/>\n");
+		out.write("</svg>\n");
+		out.write("</div>\n");
+
+		String urlVersion = questionnaireResponse.getQuestionnaire();
+		String[] urlVersionSplit = urlVersion.split("\\|");
+		String href = "/fhir/Questionnaire?url=" + urlVersionSplit[0] + "&version=" + urlVersionSplit[1];
+
+		out.write("<div>");
+		out.write("<p>\n");
+		out.write("This QuestionnaireResponse answers the Questionnaire:</br><b><a class=\"info-link "
+				+ (isCompleted ? "info-link-completed" : "info-link-progress") + "\" href=\"" + href + "\">"
+				+ urlVersion + "</b></a>");
+		out.write("</p>\n");
+		out.write("<ul class=\"info-list\">\n");
+		out.write("<li><b>State:</b> " + questionnaireResponse.getStatus().getDisplay() + "</li>\n");
+		out.write("<li><b>Process instance-id:</b> " + getProcessInstanceId(questionnaireResponse) + "</li>\n");
+
+		String lastUpdated = DATE_TIME_DISPLAY_FORMAT.format(questionnaireResponse.getMeta().getLastUpdated());
+		if (isCompleted)
+		{
+			out.write("<li><b>Completion date:</b> " + lastUpdated + "</li>\n");
+		}
+		else
+		{
+			out.write("<li><b>Creation date:</b> " + lastUpdated + "</li>\n");
+		}
+
+		out.write("</ul>\n");
+		out.write("</div>\n");
+		out.write("</div>\n");
+
+		out.write("<fieldset id=\"qr-form-fieldset\" " + (isCompleted ? "disabled=\"disabled\"" : "") + ">\n");
 
 		for (QuestionnaireResponse.QuestionnaireResponseItemComponent item : questionnaireResponse.getItem())
 		{
-			writeRow(item, out);
+			writeRow(item, isCompleted, out);
 		}
 
 		out.write("<div class=\"row row-submit\" id=\"submit-row\">\n");
-		out.write(
-				"<button type=\"button\" id=\"submit\" class=\"submit\" onclick=\"completeQuestionnaireResponse();\">Submit</button>\n");
+		out.write("<button type=\"button\" id=\"submit\" class=\"submit\" onclick=\"completeQuestionnaireResponse();\" "
+				+ (isCompleted ? "disabled" : "") + ">Submit</button>\n");
 		out.write("</div>\n");
+		out.write("</fieldset>\n");
 		out.write("</form>\n");
 	}
 
-	private void writeRow(QuestionnaireResponse.QuestionnaireResponseItemComponent item, OutputStreamWriter out)
-			throws IOException
+	private String getProcessInstanceId(QuestionnaireResponse questionnaireResponse)
+	{
+		return questionnaireResponse.getItem().stream()
+				.filter(i -> CODESYSTEM_HIGHMED_BPMN_USER_TASK_VALUE_BUSINESS_KEY.equals(i.getLinkId()))
+				.flatMap(i -> i.getAnswer().stream()).map(a -> ((StringType) a.getValue()).getValue()).findFirst()
+				.orElse("unknown");
+	}
+
+	private void writeRow(QuestionnaireResponse.QuestionnaireResponseItemComponent item, boolean isCompleted,
+			OutputStreamWriter out) throws IOException
 	{
 		String linkId = item.getLinkId();
 		String style = display(linkId) ? "" : "style=\"display:none;\"";
 
 		out.write("<div class=\"row\" id=\"" + linkId + "-row\"" + style + ">\n");
 		out.write("<label for=\"" + linkId + "-label\">" + item.getText() + "</label>\n");
-		writeFormInput(item.getAnswerFirstRep(), linkId, out);
+		writeFormInput(item.getAnswerFirstRep(), linkId, isCompleted, out);
+		out.write("<ul class=\"error-list-not-visible\" id=\"" + linkId + "-error\">\n");
+		out.write("</ul>\n");
 		out.write("</div>\n");
 	}
 
@@ -77,62 +137,72 @@ public class QuestionnaireResponseHtmlFhirAdapter extends HtmlFhirAdapter<Questi
 	}
 
 	private void writeFormInput(QuestionnaireResponse.QuestionnaireResponseItemAnswerComponent answerPlaceholder,
-			String linkId, OutputStreamWriter out) throws IOException
+			String linkId, boolean isCompleted, OutputStreamWriter out) throws IOException
 	{
 		Type type = answerPlaceholder.getValue();
 
 		if (type instanceof StringType)
 		{
-			String placeholder = ((StringType) type).getValue();
-			out.write("<input type=\"text\" id=\"" + linkId + "\" name=\"" + linkId + "\" placeholder=\"" + placeholder
-					+ "\"></input>\n");
+			String value = ((StringType) type).getValue();
+			out.write("<input type=\"text\" id=\"" + linkId + "\" name=\"" + linkId + "\" "
+					+ (isCompleted ? "value=\"" + value + "\"" : "placeholder=\"" + value + "\"") + "\"></input>\n");
 		}
 		else if (type instanceof IntegerType)
 		{
-			String placeholder = String.valueOf(((IntegerType) type).getValue());
-			out.write("<input type=\"number\" id=\"" + linkId + "\" name=\"" + linkId + "\" placeholder=\""
-					+ placeholder + "\" step=\"1\"></input>\n");
+			String value = String.valueOf(((IntegerType) type).getValue());
+			out.write("<input type=\"number\" id=\"" + linkId + "\" name=\"" + linkId + "\" step=\"1\" "
+					+ (isCompleted ? "value=\"" + value + "\"" : "placeholder=\"" + value + "\"") + "></input>\n");
 		}
 		else if (type instanceof DecimalType)
 		{
-			String placeholder = String.valueOf(((DecimalType) type).getValue());
-			out.write("<input type=\"number\" id=\"" + linkId + "\" name=\"" + linkId + "\" placeholder=\""
-					+ placeholder + "\" step=\"0.01\"></input>\n");
+			String value = String.valueOf(((DecimalType) type).getValue());
+			out.write("<input type=\"number\" id=\"" + linkId + "\" name=\"" + linkId + "\" step=\"0.01\" "
+					+ (isCompleted ? "value=\"" + value + "\"" : "placeholder=\"" + value + "\"") + "></input>\n");
 		}
 		else if (type instanceof BooleanType)
 		{
+			boolean valueIsTrue = ((BooleanType) type).getValue();
+
 			out.write("<div>\n");
 			out.write("<label class=\"radio\"><input type=\"radio\" id=\"" + linkId + "\" name=\"" + linkId
-					+ "\" value=\"true\"/>Yes</label>\n");
+					+ "\" value=\"true\" " + ((valueIsTrue) ? "checked" : "") + "/>Yes</label>\n");
 			out.write("<label class=\"radio\"><input type=\"radio\" id=\"" + linkId + "\" name=\"" + linkId
-					+ "\" value=\"false\" checked/>No</label>\n");
+					+ "\" value=\"false\" " + ((!valueIsTrue) ? "checked" : "") + "/>No</label>\n");
 			out.write("</div>\n");
 		}
 		else if (type instanceof DateType)
 		{
-			out.write("<input type=\"date\" id=\"" + linkId + "\" name=\"" + linkId
-					+ "\" placeholder=\"dd.MM.yyyy\"></input>\n");
+			Date value = ((DateType) type).getValue();
+			String date = DATE_FORMAT.format(value);
+
+			out.write("<input type=\"date\" id=\"" + linkId + "\" name=\"" + linkId + "\" "
+					+ (isCompleted ? "value=\"" + date + "\"" : "placeholder=\"yyyy.MM.dd\"") + "></input>\n");
 		}
 		else if (type instanceof TimeType)
 		{
-			out.write("<input type=\"time\" id=\"" + linkId + "\" name=\"" + linkId
-					+ "\" placeholder=\"hh:mm:ss\"></input>\n");
+			String value = ((TimeType) type).getValue();
+			out.write("<input type=\"time\" id=\"" + linkId + "\" name=\"" + linkId + "\" "
+					+ (isCompleted ? "value=\"" + value + "\"" : "placeholder=\"hh:mm:ss\"") + "></input>\n");
 		}
 		else if (type instanceof DateTimeType)
 		{
-			out.write("<input type=\"datetime-local\" id=\"" + linkId + "\" name=\"" + linkId
-					+ "\" placeholder=\"dd.MM.yyyy hh:mm:ss\"></input>\n");
+			Date value = ((DateTimeType) type).getValue();
+			String dateTime = DATE_TIME_FORMAT.format(value);
+
+			out.write("<input type=\"datetime-local\" id=\"" + linkId + "\" name=\"" + linkId + "\" "
+					+ (isCompleted ? "value=\"" + dateTime + "\"" : "placeholder=\"yyyy.MM.dd hh:mm:ss\"")
+					+ "></input>\n");
 		}
 		else if (type instanceof Reference)
 		{
-			String placeholder = ((Reference) type).getReference();
-			out.write("<input type=\"url\" id=\"" + linkId + "\" name=\"" + linkId + "\" placeholder=\"" + placeholder
-					+ "\"></input>\n");
+			String value = ((Reference) type).getReference();
+			out.write("<input type=\"url\" id=\"" + linkId + "\" name=\"" + linkId + "\" "
+					+ (isCompleted ? "value=\"" + value + "\"" : "placeholder=\"" + value + "\"") + "></input>\n");
 		}
 		else
 		{
-			throw new RuntimeException(
-					"Answer type '" + type.getClass().getName() + "' in QuestionnaireResponse.item is not supported");
+			throw new RuntimeException("Answer type '" + ((type != null) ? type.getClass().getName() : "null")
+					+ "' in QuestionnaireResponse.item is not supported");
 		}
 	}
 }
--- dsf-fhir/dsf-fhir-server/src/main/resources/static/form.js
+++ dsf-fhir/dsf-fhir-server/src/main/resources/static/form.js
@@ -2,15 +2,19 @@ function completeQuestionnaireResponse() {
     const questionnaireResponseStringBefore = document.getElementById("json").innerText
     const questionnaireResponse = JSON.parse(questionnaireResponseStringBefore)
 
-    readAnswersFromForm(questionnaireResponse)
+    const errors = []
+    readAnswersFromForm(questionnaireResponse, errors)
 
     console.log(questionnaireResponse)
-    const questionnaireResponseStringAfter = JSON.stringify(questionnaireResponse)
+    console.log(errors)
 
-    updateQuestionnaireResponse(questionnaireResponseStringAfter)
+    if (errors.length === 0) {
+        const questionnaireResponseStringAfter = JSON.stringify(questionnaireResponse)
+        updateQuestionnaireResponse(questionnaireResponseStringAfter)
+    }
 }
 
-function readAnswersFromForm(questionnaireResponse) {
+function readAnswersFromForm(questionnaireResponse, errors) {
     questionnaireResponse.status = "completed";
 
     questionnaireResponse.item.forEach((item) => {
@@ -19,28 +23,150 @@ function readAnswersFromForm(questionnaireResponse) {
         const answerType = Object.keys(answer)[0]
 
         if (id !== "business-key" && id !== "user-task-id") {
-            answer[answerType] = readValue(id, answerType)
+            answer[answerType] = readAndValidateValue(id, answerType, errors)
         }
     })
 }
 
-function readValue(id, answerType) {
-    if (['valueString', 'valueInteger', 'valueDecimal', 'valueDate', 'valueReference'].includes(answerType)) {
-        return document.getElementById(id).value
+function readAndValidateValue(id, answerType, errors) {
+    const value = document.getElementById(id).value
+
+    const rowElement = document.getElementById(id + "-row");
+    const errorListElement = document.getElementById(id + "-error");
+    errorListElement.replaceChildren()
+
+    if (answerType === 'valueString') {
+        return validateString(rowElement, errorListElement, value, errors, id)
+    } else if (answerType === 'valueInteger') {
+        return validateInteger(rowElement, errorListElement, value, errors, id)
+    } else if (answerType === 'valueDecimal') {
+        return validateDecimal(rowElement, errorListElement, value, errors, id)
+    } else if (answerType === 'valueDate') {
+        return validateDate(rowElement, errorListElement, value, errors, id)
     } else if (answerType === 'valueTime') {
-        return document.getElementById(id).value + ":00"
+        return validateTime(rowElement, errorListElement, value, errors, id)
     } else if (answerType === 'valueDateTime') {
-        return new Date(document.getElementById(id).value).toISOString()
+        return validateDateTime(rowElement, errorListElement, value, errors, id)
+    } else if (answerType === 'valueReference') {
+        return validateReference(rowElement, errorListElement, value, errors, id)
     } else if (answerType === 'valueBoolean') {
         return document.querySelector("input[name=" + id + "]:checked").value
     } else {
-        return "unknown"
+        return null
     }
+}
+
+function validateString(rowElement, errorListElement, value, errors, id) {
+    if (value === null || value.trim() === "") {
+        addError(rowElement, errorListElement, errors, id, "Value is null or empty")
+        return null
+    } else {
+        removeError(rowElement, errorListElement)
+        return value
+    }
+}
+
+function validateInteger(rowElement, errorListElement, value, errors, id) {
+    validateString(rowElement, errorListElement, value, errors, id)
+
+    if (!Number.isInteger(parseInt(value))) {
+        addError(rowElement, errorListElement, errors, id, "Value is not an integer")
+        return null
+    } else {
+        removeError(rowElement, errorListElement)
+        return value
+    }
+}
+
+function validateDecimal(rowElement, errorListElement, value, errors, id) {
+    validateString(rowElement, errorListElement, value, errors, id)
+
+    if (isNaN(parseFloat(value))) {
+        addError(rowElement, errorListElement, errors, id, "Value is not a decimal")
+        return null
+    } else {
+        removeError(rowElement, errorListElement)
+        return value
+    }
+}
+
+function validateDate(rowElement, errorListElement, value, errors, id) {
+    validateString(rowElement, errorListElement, value, errors, id)
+
+    const date = new Date(value)
+    if ((date === "Invalid Date") || isNaN(date)) {
+        addError(rowElement, errorListElement, errors, id, "Value is not a date")
+        return null
+    } else {
+        removeError(rowElement, errorListElement)
+        return value
+    }
+}
+
+function validateTime(rowElement, errorListElement, value, errors, id) {
+    validateString(rowElement, errorListElement, value, errors, id)
+
+    if (!(new RegExp('^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$').test(value))) {
+        addError(rowElement, errorListElement, errors, id, "Value is not a time")
+        return null
+    } else {
+        removeError(rowElement, errorListElement)
+        return value + ":00"
+    }
+}
+
+function validateDateTime(rowElement, errorListElement, value, errors, id) {
+    validateString(rowElement, errorListElement, value, errors, id)
+
+    try {
+        const dateTime = new Date(value).toISOString()
+        removeError(rowElement, errorListElement)
+        return dateTime
+    } catch (_) {
+        addError(rowElement, errorListElement, errors, id, "Value is not a date time")
+        return null
+    }
+}
+
+function validateReference(rowElement, errorListElement, value, errors, id) {
+    validateString(rowElement, errorListElement, value, errors, id)
+
+    try {
+        new URL(value);
+        removeError(rowElement, errorListElement)
+        return {reference: value}
+    } catch (_) {
+        addError(rowElement, errorListElement, errors, id, "Value is not a reference")
+        return null
+    }
+}
+
+function addError(rowElement, errorListElement, errors, id, message) {
+    errors.push({id: id, error: message})
+
+    rowElement.classList.add("error")
+
+    const errorMessageElement = document.createElement("li");
+    errorMessageElement.appendChild(document.createTextNode(message));
+
+    errorListElement.appendChild(errorMessageElement);
+    errorListElement.classList.remove("error-list-not-visible");
+    errorListElement.classList.add("error-list-visible");
+}
+
+function removeError(rowElement, errorListElement) {
+    rowElement.classList.remove("error");
+
+    errorListElement.classList.remove("error-list-visible");
+    errorListElement.classList.add("error-list-not-visible");
+    errorListElement.replaceChildren()
 }
 
 function updateQuestionnaireResponse(questionnaireResponse) {
     const fullUrl = window.location.origin + window.location.pathname
     const url = fullUrl.slice(0, fullUrl.indexOf("/_history") + 1)
+
+    enableSpinner()
 
     fetch(url, {
         method: "PUT",
@@ -50,5 +176,32 @@ function updateQuestionnaireResponse(questionnaireResponse) {
         body: questionnaireResponse
     }).then(response => {
         console.log(response)
+
+        if (response.ok) {
+            disableSpinner()
+            window.scrollTo(0, 0);
+            location.reload();
+        } else {
+            const status = response.status
+            const statusText = response.statusText === null ? " - " + response.statusText : ""
+
+            response.text().then((responseText) => {
+                const alertText = "Status: " + status + statusText + "\n\n" + responseText.replace(/<!--.*?-->/sg, "")
+                window.alert(alertText);
+                disableSpinner()
+            })
+        }
     })
+}
+
+function enableSpinner() {
+    const spinner = document.getElementById("spinner")
+    spinner.classList.remove("spinner-disabled")
+    spinner.classList.add("spinner-enabled")
+}
+
+function disableSpinner() {
+    const spinner = document.getElementById("spinner")
+    spinner.classList.remove("spinner-enabled")
+    spinner.classList.add("spinner-disabled")
 }
